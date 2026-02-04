aa13585fb4afad332d24ada4a461a5bde7859afd
ntstpnv <greatinfoonly13@gmail.com>
2026-02-05 00:06:14 +0500

    +=

--- crud.py
+++ crud.py
@@ -1,7 +1,22 @@
 import json
 
 
-def update_group():
+def update_log() -> None:
+    with open("log.json", "r+", encoding="utf-8") as file:
+        log = json.load(file)
+
+        new = {}
+        for g, fn_dict in log.items():
+            for fn, ti_dict in fn_dict.items():
+                for ti, r_list in ti_dict.items():
+                    pass
+
+        file.seek(0)
+        json.dump(new, file, ensure_ascii=False, indent=None, separators=(",", ":"))
+        file.truncate()
+
+
+def update_students() -> None:
     with open("students.json", "r+", encoding="utf-8") as file:
         students = json.load(file)
 
@@ -13,4 +28,4 @@ def update_group():
 
 
 if __name__ == "__main__":
-    update_group()
+    pass
